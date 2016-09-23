b057cd08e8b3a99a4efae58831c87c6715a9d77a
Sean Xun Luo <SXunLuo@webershandwick.com>
2016-09-23 14:10:29 -0400

    QA updates

--- ws-homepage/js/scripts.js
+++ ws-homepage/js/scripts.js
@@ -2,6 +2,18 @@ $(document).ready(function() {
 
 	checkSize();
     $(window).resize(checkSize);
+	
+	$(".jump-link").click(function(event) {
+		event.preventDefault();
+		var _which = $(this).attr("href");
+		$("html,body").animate({
+			scrollTop: $(_which).offset().top
+		}, 500, function() {
+			window.location.hash = _which;
+		});
+	});
+	
+	$(".load-overlay-video").fancybox();
 
 });
 
@@ -29,8 +41,17 @@ function checkSize() {
         $(".hemnet .person-container").detach().appendTo(".hemnet .position-right");
         $(".barbie .person-container").detach().appendTo(".barbie .position-right");
 
-        $(".who-we-are .ws-sec-content").detach().insertBefore(".who-we-are h2");
-        $(".who-we-are .ws-sec-container").detach().insertAfter(".who-we-are .tablet-right");
+        //$(".who-we-are .ws-sec-content").detach().insertBefore(".who-we-are h2");
+        //$(".who-we-are .ws-sec-container").detach().insertAfter(".who-we-are .tablet-right");
     
     }
+	
+	$(".full-width-background").each(function() {
+		var _this = $(this);
+		var _margin_left = -1 * ( ( $(window).width() - _this.outerWidth(true) ) / 2 );
+		$(_this).find(".background-color").css({
+			width: $(window).width() + "px",
+			"margin-left": _margin_left + "px"
+		});
+	});
 }
